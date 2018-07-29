0f978d91b755d675b72686c7d4565a3443d117cd
Mike UZAN <uzan_mike@sansentinel.com>
2018-07-29 22:51:59 +0200

    added streaming func to Kafka topic:frames with images in String format (base64)

--- stream_drone_video.py
+++ stream_drone_video.py
@@ -1,26 +1,27 @@
-import sys,traceback,argparse,requests
+import sys,traceback,argparse,requests,base64
 import tellopy
 import av
 import time
 import image
+import cStringIO
+from requests.utils import quote
 
 
 kafka_stream_header = {'Content-type': 'application/vnd.kafka.json.v1+json'}
 
-def stream(img_str):
-    payload='{"records":[{"value":{'
-    payload+='"frame":"' + frame + '"'
-    payload += '}}]}'
-    r = requests.post(kafka_stream_url, headers=kafka_stream_header, data=payload)
-    r
-    r.json()
+def stream_frame(img_name_str, img_data_str):
+    frame_stream_url = args.kakfa_rest_gateway + quote(args.kakfa_stream, safe='') + quote(':') + 'frames'
+    print('### STREAMING TO: ' + frame_stream_url)
+    frame_payload = '{"records":[{"value":{"img_name":"' + img_name_str + '","img_data":"' + img_data_str + '"}}]}'
+    r = requests.post(frame_stream_url, headers=kafka_stream_header, data=frame_payload)
+    print(r)
+    print(r.json())
 
 def main():
     drone = tellopy.Tello()
     try:
         drone.connect()
-        drone.wait_for_connection(60.0)
-
+        drone.wait_for_connection(120.0)
         container = av.open(drone.get_video_stream())
         # skip first 300 frames
         frame_skip = 300
@@ -30,12 +31,24 @@ def main():
                     frame_skip = frame_skip - 1
                     continue
                 start_time = time.time()
-                img = frame.to_image()
-                img.save('frame-%04d.jpg' % frame.index)
                 # Streaming is ON
                 if stream_args:
-                    stream(str(image))
+                    # Store Image name
+                    img_name_str = 'frame-%06d.jpg' % frame.index
+                    print('### BEGIN NAME STRING ###')
+                    print(img_name_str)
+                    print('### END NAME STRING ###')
+                    # Convert Bin image to String
+                    buffer = cStringIO.StringIO()
+                    frame.to_image().save(buffer, format="JPEG")
+                    img_data_str = base64.b64encode(buffer.getvalue())
+                    print('### BEGIN DATA STRING ###')
+                    print(img_data_str)
+                    print('### END DATA STRING ###')
+                    stream_frame(str(img_name_str),str(img_data_str))
+
                 frame_skip = int((time.time() - start_time)/frame.time_base)
+                #time.sleep(5)
 
     except Exception as ex:
         exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -49,13 +62,12 @@ if __name__ == '__main__':
     group = parser.add_argument_group('stream')
     group.add_argument('-g', '--kafka-rest-gw', dest='kakfa_rest_gateway', type=str, default='', help='URL of Kafka REST gateway.')
     group.add_argument('-s', '--kafka-stream', dest='kakfa_stream', type=str, default='', help='Kafka stream name.')
-    group.add_argument('-t', '--kafka-topic', dest='kakfa_topic', type=str, default='', help='Kafka topic.')
     args = parser.parse_args()
 
     stream_args = False
-    if args.kakfa_rest_gateway and args.kakfa_stream and args.kakfa_topic:
+    if args.kakfa_rest_gateway and args.kakfa_stream :
         stream_args = True
-        kafka_stream_url = args.kakfa_rest_gateway + quote(args.kakfa_stream, safe='') + quote(':') + args.kakfa_topic
-        print ('Streaming to : ' + str(kafka_stream_url))
+        topic_url = args.kakfa_rest_gateway + quote(args.kakfa_stream, safe='')
+        print ('Streaming to topic : ' + str(topic_url))
 
     main()
