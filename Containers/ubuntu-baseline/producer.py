59bd6e88875d5ad954d6f9f82046053aa525bfa7
Mike UZAN <uzan_mike@sansentinel.com>
2018-08-03 17:39:11 +0200

    cleaning

--- Containers/ubuntu-baseline/producer.py
+++
@@ -1,6 +0,0 @@
-from confluent_kafka import Producer
-p = Producer({'streams.producer.default.stream': '/demos/drone/drone1'})
-some_data_source= ["msg1", "msg2", "msg3"]
-for data in some_data_source:
-     p.produce('frames', data.encode('utf-8'))
-     p.flush()
