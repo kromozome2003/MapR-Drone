bce6257576c3e21ce017d6ce56a87ca93166a22b
muzan <muzan@a1z2e3r4t5y6.local>
2018-08-08 18:48:18 +0200

    Working streaming container. Yeahhh

--- Containers/drone-client/docker_images/client/mapr-docker-client.sh
+++ Containers/drone-client/docker_images/client/mapr-docker-client.sh
@@ -68,4 +68,4 @@ MAPR_DOCKER_ARGS="$MAPR_DOCKER_SECURITY \
   -v $MAPR_TICKET_FILE:$MAPR_TICKETFILE_LOCATION:ro"
 [ -d /sys/fs/cgroup ] && MAPR_DOCKER_ARGS="$MAPR_DOCKER_ARGS -v /sys/fs/cgroup:/sys/fs/cgroup:ro"
 
-docker run --rm -it $MAPR_DOCKER_ARGS drone-client:latest "$@"
+docker run --rm -it -p 5000:5000 $MAPR_DOCKER_ARGS drone-client:latest "$@"
